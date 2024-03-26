8192308f21e32de03a2220449951ad08a2621e3b
Suryansh Sharma <suryansh.sharma9315@gmail.com>
2024-03-26 16:30:31 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,9 +17,6 @@ export default defineConfig({
       cache: false,
     },
     outDir: "dist",
-  },
-  resolve: {
-    jsbi: path.resolve(__dirname, "node_modules/jsbi"),
   },
   optimizeDeps: {
     esbuildOptions: {
