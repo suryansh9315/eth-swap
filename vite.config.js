954a69004979eff49f6ff6844ea6ed280070cabc
Suryansh Sharma <suryansh.sharma9315@gmail.com>
2024-03-26 16:27:42 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-})
+  build: {
+    minify: false,
+    target: "es2020", // Enable Big integer literals
+    commonjsOptions: {
+      transformMixedEsModules: true, // Enable @walletconnect/web3-provider which has some code in CommonJS
+    },
+
+    rollupOptions: {
+      // maxParallelFileOps: 2,
+      cache: false,
+    },
+    outDir: "dist",
+  },
+  resolve: {
+    jsbi: path.resolve(__dirname, "node_modules/jsbi"),
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2020", // Enable Big integer literals
+      define: {
+        global: "globalThis",
+      },
+      supported: {
+        bigint: true,
+      },
+    },
+  },
+});
